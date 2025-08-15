ff3440d24ab7f1d0029d82b27a021bab2c44b32c
Ankith <82178043+AnkithVarri@users.noreply.github.com>
2025-08-15 10:16:38 +0100

    Update subproblem2.py

--- subproblem2.py
+++ subproblem2.py
@@ -1,92 +1,69 @@
 from tkinter import *
 from tkinter import ttk
+import math
 import random
+import matplotlib.pyplot as plt
+
+import random
+import tkinter as tk
+from tkinter import Scale, Label
+
 class CreateParticle:
-    def __init__(self,root,subcanvas1, subcanvas2,  get_colour_method, target_shape = None, trigger_callback = None):
+    def __init__(self, root, subcanvas1, trigger_callback=None,get_colour_method=None):
         self.root = root
         self.subcanvas1 = subcanvas1
         self.moving_photons = []
-        self.releasecoords = (509,550)
+        self.releasecoords = (509, 550)
         self.get_colour_method = get_colour_method
+        self.target_shape = self.subcanvas1.create_rectangle(777, 280, 782, 480, fill="grey", outline="black", width=2)
+        self.trigger_callback = trigger_callback
+        self.coords = (230, 280, 235, 480)
+        self.running = False
         self.time = 1000
 
-        self.play_event = True
-        self.running = False        
-        self.target_shape = target_shape
-
-        self.trigger_callback = trigger_callback
-        self.coords = (230,280,235,480)
-
-    def create_photon(self):
-        if not self.play_event:  # Only create new photons if play_event is True
-            return  # Don't create a new photon if paused
-        radius = 2.5 
-        x1 = random.randint(self.releasecoords[0], self.releasecoords[1])  # Start just outside the left edge of the canvas
+    def create_photon(self, colour =" #737373"):
+        radius = 2.5
+        x1 = random.randint(self.releasecoords[0], self.releasecoords[1])
         y1 = x1 - 410
         x2 = x1 + radius * 2
         y2 = y1 + radius * 2
-        coords = (y1,y2)
-
-        colour = self.get_colour_method()
-
-        # Draw the circle and return its ID
         circle_id = self.subcanvas1.create_oval(x1, y1, x2, y2, fill=colour, outline="")
         self.moving_photons.append(circle_id)
-        return coords
-    
-
-    #Intensity links to the number of particles produced per second
-
 
     def move_photons(self):
         if not self.running:
             return
-        
-        if self.play_event:
-            for circle_id in self.moving_photons[:]:  # Iterate over a copy of the list
-                self.move_pattern(circle_id)  
+        for circle_id in self.moving_photons[:]:
+            self.move_pattern(circle_id)
+            coords = self.subcanvas1.coords(circle_id)
+            if coords:
+                x1, y1, x2, y2 = coords
+                if self.coords:
+                    if (x1 < self.coords[2] and x2 > self.coords[0] and y1 < self.coords[3] and y2 > self.coords[1]):
+                        if self.trigger_callback:
+                            self.trigger_callback((x1, y1))
+                if x1 < 225:
+                    self.subcanvas1.delete(circle_id)
+                    self.moving_photons.remove(circle_id)
+        self.subcanvas1.after(50, self.move_photons)
 
-                coords = self.subcanvas1.coords(circle_id)  # Get the coordinates
-                if coords:  # Ensure the object exists
-                    x1, y1, x2, y2 = coords
-                    # Check for collision with the target shape
-                    if self.target_shape and self.coords:
-                        if (x1 < self.coords[2] and x2 > self.coords[0] and y1 < self.coords[3] and y2 > self.coords[1]):
-                            if self.trigger_callback:
-                                self.trigger_callback((x1, y1)) # Trigger the second particle motion
-                    
-                    # Check if the particle goes out of bounds
-                    if x1 < 225:
-                        self.subcanvas1.delete(circle_id)  
-                        self.moving_photons.remove(circle_id)  
-            self.subcanvas1.after(50, self.move_photons) # Schedule the next movement
-
-
-
-    def move_pattern(self,circle_id):
-       self. subcanvas1.move(circle_id, -15, 10)
-
+    def move_pattern(self, circle_id):
+        self.subcanvas1.move(circle_id, -15, 10)
 
     def start(self):
-        for i in range(50):
-            print("done")
-        if self.running == True:
-            self.create_photon()  # Create an initial particle
-            self.move_photons()  # Start moving particles
-            self.subcanvas1.after(50, self.continuous_particle_creation)
+            colour = self.get_colour_method()
+            self.create_photon(colour)
+            self.move_photons()
+            self.subcanvas1.after(50, self.continuous_photon_creation)
 
-    def continuous_particle_creation(self):
-        """Continuously create new particles while running."""
-        print("say less")
-        if self.running:
-            self.create_photon()
-            new_time = int(1000 /self.time)
-            self.subcanvas1.after(new_time, self.continuous_particle_creation)  #the INTENSITY SLIDER BIT
-
+    def continuous_photon_creation(self):
+            colour = self.get_colour_method()
+            self.create_photon(colour)
+            new_time = int(1000 / self.time)
+            self.after_id = self.subcanvas1.after(new_time, self.continuous_photon_creation)
 
     def update_time(self, value):
-        self.time = max(1, int(value))  # Ensure time is at least 1 ms
+        self.time = max(1, int(value))
 
-    def update_colour(self, colour):
-        self.colour = colour # Changs colour to the colour chosen by the slider
-
+    def stop(self):
+        self.running = False
